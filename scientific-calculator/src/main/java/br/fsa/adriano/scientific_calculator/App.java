9bed9c2154c3b1f6b320f122890750c2d81f3081
mantinha <adriandesigner@hotmail.com>
2020-10-01 03:56:36 -0300

    calculadora.fxml Criado!

--- scientific-calculator/src/main/java/br/fsa/adriano/scientific_calculator/App.java
+++ scientific-calculator/src/main/java/br/fsa/adriano/scientific_calculator/App.java
@@ -17,9 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("calculadora"), 600, 400);
+        stage.setTitle("Scientific Calculator");
         stage.setScene(scene);
-        stage.show();
+        stage.show();        
     }
 
     static void setRoot(String fxml) throws IOException {
--- scientific-calculator/src/main/java/module-info.java
+++ scientific-calculator/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.fsa.adriano.scientific_calculator {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.desktop;
+	requires javafx.swing;
 
     opens br.fsa.adriano.scientific_calculator to javafx.fxml;
     exports br.fsa.adriano.scientific_calculator;
